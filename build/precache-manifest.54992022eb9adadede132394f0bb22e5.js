a750b8abd1a0281bb6721482eea4b5d8fe24c50f
shangchunli <1503651223@qq.com>
2019-11-07 16:46:23 +0800

    message

---
+++ build/precache-manifest.54992022eb9adadede132394f0bb22e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb6046ab41eae644ea18f2ab34d05b76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b5860f9db7d904122c3",
+    "url": "/static/css/2.0b726e0e.chunk.css"
+  },
+  {
+    "revision": "91ba9bc1824f7e43d84e",
+    "url": "/static/css/main.86908a0f.chunk.css"
+  },
+  {
+    "revision": "5b5860f9db7d904122c3",
+    "url": "/static/js/2.e0829f6b.chunk.js"
+  },
+  {
+    "revision": "91ba9bc1824f7e43d84e",
+    "url": "/static/js/main.8a32e401.chunk.js"
+  },
+  {
+    "revision": "628ea25a63cbc5b89a27",
+    "url": "/static/js/runtime-main.9f147cbc.js"
+  }
+]);
